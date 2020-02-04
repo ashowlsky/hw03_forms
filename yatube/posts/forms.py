da14954a85832cfe72ea02137226bb4802fc74c9
Александр Шаульский <ashowlsky@yandex.ru>
2020-02-04 20:38:37 +0300

    what attempt

---
+++ yatube/posts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from posts.models import Post, Group
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('text', 'group')
+
+    def check_text(self):
+        checked_text = self.cleaned_data['text']
+        if checked_text is None:
+            raise forms.ValidationError("Пост должен содержать текст")
+        return checked_text
+
+
---
+++ yatube/posts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import Post, Group
+from .forms import PostForm
+
+
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:11]
+    return render(request, "index.html", {"posts": latest})
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group = group).order_by("-pub_date")[:12]
+    return render(request, "group.html", {"group":group, "posts":posts})
+
+def new_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            Post.text = form.cleaned_data['text']
+            Post.group = form.cleaned_data['group']
+            return redirect('index')
+        return render(request, 'new.html', {'form':form})
+    form = PostForm()
+    return render(request, 'new.html', {'form':form})
+    
